6dfbf1fbc05fd028e2ccbe7ab1fa3f048fef4b9a
Семён Вервейн <semen.vervein@gmail.com>
2021-12-19 15:19:12 +0700

    Connection

--- Authorization.cs
+++ Authorization.cs
@@ -8,21 +8,6 @@ namespace OtdelZasel
 {
     public partial class Authorization : Form
     {
-        private string connectstr = String.Format(
-            "Server={0};" +
-            "Port={1};" +
-            "User Id={2};" +
-            "Password={3};" +
-            "Database={4};", 
-            "rds-postgresql-otdel-zasel.cguxtch14lq2.eu-west-2.rds.amazonaws.com",
-            5432,
-            "Nikolai",
-            "kkjBB334HE3#4bh",
-            "Check_In_Department");
-
-        private NpgsqlConnection connection;
-
-
         public Authorization()
         {
             InitializeComponent();
@@ -30,18 +15,20 @@ namespace OtdelZasel
 
         private void Authorization_Load(object sender, EventArgs e)
         {
-            connection = new NpgsqlConnection(connectstr);
+            
         }
 
         private void Auth_Button_Click(object sender, EventArgs e)
         {
             try
             {
-                connection.Open();
+                Connection.getInstance().connection.Open();
                 var sql = @"select * from loginuser(:login, :password)";
-                var cmd = new NpgsqlCommand(sql,connection);
-                cmd.Parameters.AddWithValue("login", login.Text);
-                cmd.Parameters.AddWithValue("password", password.Text);
+                var cmd = new NpgsqlCommand(sql, Connection.getInstance().connection);
+                //cmd.Parameters.AddWithValue("login", login.Text);
+                //cmd.Parameters.AddWithValue("password", password.Text);
+                cmd.Parameters.AddWithValue("login", "ivankod");
+                cmd.Parameters.AddWithValue("password", "qwe");
                 var auth = cmd.ExecuteReader();
                 auth.Read();
                 int role = (int)auth.GetValue(0);
@@ -66,12 +53,11 @@ namespace OtdelZasel
                 {
                     MessageBox.Show("Неправильный логин или пароль");
                 }
-                connection.Close();
-
+                Connection.getInstance().connection.Close();
             }
             catch (Exception ex)
             {
-                connection.Close();
+                Connection.getInstance().connection.Close();
                 MessageBox.Show("Auth fail. Error: " + ex.Message);
                 throw;
             }
