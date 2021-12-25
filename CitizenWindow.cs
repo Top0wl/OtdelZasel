42cd8bf70eda7d59dff49a9ab7762cda40fa8899
VladBel2000 <vlad.belomestnov.00@mail.ru>
2021-12-25 21:29:54 +0300

    Исправлена ошибки при подаче заявления от незаселённого пользователя

--- CitizenWindow.cs
+++ CitizenWindow.cs
@@ -51,58 +51,72 @@ namespace OtdelZasel
         // Подача заявления на выселение
         private void button_send_PetitonOut_Click(object sender, EventArgs e)
         {
-
-            try
+            Connection.getInstance().connection.Open();
+            //SQL команда
+            var sql = @"select * from isLiving(:id_citizen)";
+            //Подключние команды
+            var cmd = new NpgsqlCommand(sql, Connection.getInstance().connection);
+            cmd.Parameters.AddWithValue("id_citizen", ID_Citizen);
+            bool have_citizen = Convert.ToBoolean(cmd.ExecuteScalar());
+            Connection.getInstance().connection.Close();
+            if (have_citizen)
             {
-                long id_leaving_reason = 0;
-                Connection.getInstance().connection.Open();
-                //SQL команда
-                var sql_1 = "select * from \"LeavingReason\"";
-                //Подключние команды
-                var cmd_1 = new NpgsqlCommand(sql_1, Connection.getInstance().connection);
-                var dt = new DataTable();
-                dt.Load(cmd_1.ExecuteReader());
-                foreach (DataRow row in dt.Rows)
-                {
-                    if (comboBox_LeavingReason.Text == row[dt.Columns[1]].ToString())
-                        id_leaving_reason = (long)row[dt.Columns[0]];
-                }
-                Connection.getInstance().connection.Close();
-
                 try
                 {
-                    // Посмотреть вызов функции и передать параметр, который отвечает за причину выселения
-                    //Обязательный коннект
+                    long id_leaving_reason = 0;
                     Connection.getInstance().connection.Open();
                     //SQL команда
-                    var sql = @"select * from createcheckoutpetition(:text, :id_citizen, :id_leavingreason)";
+                    sql = "select * from \"LeavingReason\"";
                     //Подключние команды
-                    var cmd = new NpgsqlCommand(sql, Connection.getInstance().connection);
-
-                    //Параметры
+                    cmd = new NpgsqlCommand(sql, Connection.getInstance().connection);
+                    var dt = new DataTable();
+                    dt.Load(cmd.ExecuteReader());
+                    foreach (DataRow row in dt.Rows)
                     {
-                        cmd.Parameters.AddWithValue("text", richTextBox_PetitonOut.Text);
-                        cmd.Parameters.AddWithValue("id_citizen", ID_Citizen);
-                        cmd.Parameters.AddWithValue("id_leavingreason", id_leaving_reason);
+                        if (comboBox_LeavingReason.Text == row[dt.Columns[1]].ToString())
+                            id_leaving_reason = (long)row[dt.Columns[0]];
                     }
-                    var petition = cmd.ExecuteScalar();
                     Connection.getInstance().connection.Close();
-                    MessageBox.Show("Заявление на выселение успешно подано");
-                    richTextBox_PetitonOut.Text = "";
-                    comboBox_LeavingReason.Text = "";
+
+                    try
+                    {
+                        // Посмотреть вызов функции и передать параметр, который отвечает за причину выселения
+                        //Обязательный коннект
+                        Connection.getInstance().connection.Open();
+                        //SQL команда
+                        sql = @"select * from createcheckoutpetition(:text, :id_citizen, :id_leavingreason)";
+                        //Подключние команды
+                        cmd = new NpgsqlCommand(sql, Connection.getInstance().connection);
+
+                        //Параметры
+                        {
+                            cmd.Parameters.AddWithValue("text", richTextBox_PetitonOut.Text);
+                            cmd.Parameters.AddWithValue("id_citizen", ID_Citizen);
+                            cmd.Parameters.AddWithValue("id_leavingreason", id_leaving_reason);
+                        }
+                        var petition = cmd.ExecuteScalar();
+                        Connection.getInstance().connection.Close();
+                        MessageBox.Show("Заявление на выселение успешно подано");
+                        richTextBox_PetitonOut.Text = "";
+                        comboBox_LeavingReason.Text = "";
+                    }
+                    catch (Exception ex)
+                    {
+                        Connection.getInstance().connection.Close();
+                        MessageBox.Show("Auth fail. Error: " + ex.Message);
+                        throw;
+                    }
                 }
                 catch (Exception ex)
                 {
                     Connection.getInstance().connection.Close();
-                    MessageBox.Show("Auth fail. Error: " + ex.Message);
+                    MessageBox.Show("Не выбрана причина выселения");
                     throw;
                 }
             }
-            catch (Exception ex)
+            else
             {
-                Connection.getInstance().connection.Close();
-                MessageBox.Show("Auth fail. Error: " + ex.Message);
-                throw;
+                MessageBox.Show("В данный момент вы не заселены");
             }
         }
         private void CitizenWindow_Load(object sender, EventArgs e)
